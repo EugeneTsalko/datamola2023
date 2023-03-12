a977a960db1c8102ac258a7003cd51a6ceb90e51
EugeneTsalko <tefworkmail@gmail.com>
2023-03-12 16:43:49 +0300

    refactor: delete scripts folder from ui

--- UI/scripts/utils/commentSchema.mjs
+++
@@ -1,9 +0,0 @@
-import { checkStr, checkIsLoginValid } from './utils.mjs';
-import { TASK_MAX_LENGTH } from '../constants/constants.mjs';
-
-export const commentSchema = {
-  id: (value) => checkStr(value),
-  text: (value) => checkStr(value, TASK_MAX_LENGTH.comment),
-  createdAt: (value) => value instanceof Date,
-  author: (value) => checkIsLoginValid(value),
-};
