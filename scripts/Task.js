da74afad92971a5c7a787ed417413f285a20ad59
EugeneTsalko <tefworkmail@gmail.com>
2023-03-16 18:51:44 +0300

    feat: add basic Task class

--- scripts/Task.js
+++ scripts/Task.js
@@ -1,5 +1,59 @@
+import tasks from './mockData/mockTasks.js';
+import taskSchema from './utils/taskSchema.js';
+import {
+  checkIsObj, generateId, getCustomError, validateObjBySchema,
+} from './utils/utils.js';
+
 class Task {
-  constructor(name) {
+  constructor(name, description, assignee, status, priority, isPrivate, comments) {
+    this._id = generateId(tasks);
     this.name = name;
+    this.description = description;
+    this._createdAt = new Date();
+    this.assignee = assignee;
+    this.status = status;
+    this.priority = priority;
+    this.isPrivate = isPrivate || false;
+    this.comments = comments || [];
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get createdAt() {
+    return this._createdAt;
+  }
+
+  static validate(task) {
+    try {
+      if (!checkIsObj(task)) {
+        throw new Error(getCustomError.invalidObjParam('task', 'validateTask'));
+      }
+
+      const error = validateObjBySchema(task, taskSchema, 'validateTask');
+
+      if (error) {
+        throw error;
+      }
+
+      console.log('Task is valid!');
+
+      return true;
+    } catch (err) {
+      console.error(err.message);
+
+      return false;
+    }
   }
 }
+
+// const task = new Task();
+
+// console.log(task);
+
+// console.log(Task.validate(task));
+
+// // task.id = 1;
+// console.log(task.id);
+// console.log(task._id);
--- scripts/utils/taskSchema.js
+++ scripts/utils/taskSchema.js
@@ -2,10 +2,10 @@ import { checkStr, checkIsLoginValid } from './utils.js';
 import { TASK_MAX_LENGTH, TASK_PRIORITY, TASK_STATUS } from '../constants/constants.js';
 
 const taskSchema = {
-  id: (value) => checkStr(value),
+  _id: (value) => checkStr(value),
   name: (value) => checkStr(value, TASK_MAX_LENGTH.name),
   description: (value) => checkStr(value, TASK_MAX_LENGTH.description),
-  createdAt: (value) => value instanceof Date,
+  _createdAt: (value) => value instanceof Date,
   assignee: (value) => checkIsLoginValid(value),
   status: (value) => Object.values(TASK_STATUS).includes(value),
   priority: (value) => Object.values(TASK_PRIORITY).includes(value),
