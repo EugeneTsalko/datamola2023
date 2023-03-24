a005c760a8fb4dabc0ca6919aa38b4f5e9b8b7ff
EugeneTsalko <tefworkmail@gmail.com>
2023-03-24 15:26:12 +0300

    refactor: add basic FilterView

--- scripts/view/components/Filter/FilterView.js
+++ scripts/view/components/Filter/FilterView.js
@@ -1,61 +1,24 @@
 class FilterView {
   constructor(parentId) {
     this.root = document.getElementById(parentId);
-    this.node = DomHelper.createNode('div', ['menu'], { id: 'menu' });
   }
 
   display(params) {
     try {
-      const addTaskBtn = DomHelper.createNode('button', [
-        'btn',
-        'secondary-btn',
-        'add-task-btn',
-        'hidden',
-      ]);
-      addTaskBtn.innerHTML = '<img src="./assets/svg/add.svg" alt="add"><span>ADD TASK</span>';
-
-      const filterContent = `
-      <nav>
-        <div class="filter-container">
-          <button class="btn secondary-btn icon-btn filter-btn"></button>
-          <div class="filter-bar">
-            <div class="checkbox-dropdown">
-              <span>Assignee</span>
-            </div>
-            <div class="checkbox-dropdown">
-              <span>Date</span>
-            </div>
-            <div class="checkbox-dropdown">
-              <span>Privacy</span>
-            </div>
-            <div class="checkbox-dropdown">
-              <span>Priority</span>
-            </div>
-          </div>
-          <form class="search-form">
-            <input type="text" class="search" placeholder="Search...">
-            <button type="submit" class="search-btn"></button>
-          </form>
-        </div>
-        <div class="view-container">
-          <span>View:</span>
-          <button class="btn secondary-btn kanban-btn active"></button>
-          <button class="btn secondary-btn table-btn"></button>
-        </div>
-      </nav>`;
-
       if (checkIsObj(params)) {
-        const { isAuth } = params;
+        const { isAuth, assignees } = params;
 
         if (isAuth) {
-          addTaskBtn.classList.remove('hidden');
+          document.getElementById('addTaskBtn').classList.remove('hidden');
+        }
+
+        if (assignees) {
+          // TODO сделать прием ассайни из массива тасок
+          this.root.append();
         }
       }
-      this.node.innerHTML = addTaskBtn.outerHTML + filterContent;
-
-      this.root.prepend(this.node);
     } catch (err) {
-      console.err(err.message);
+      console.error(err.message);
     }
   }
 }
