1413d16344d2e23f4a70aa11079e712a04fe7049
EugeneTsalko <tefworkmail@gmail.com>
2023-03-26 16:35:44 +0300

    feat: solve extra-task 5.2

--- extra-tasks/5.2-createList/script.js
+++ extra-tasks/5.2-createList/script.js
@@ -1,1 +1,60 @@
-console.log('hello world');
+function createListContent(list) {
+  let listContent = '';
+
+  list.forEach((el) => {
+    listContent += `<li>${el.value}</li>`;
+
+    if (!el.children) {
+      return el.value;
+    }
+
+    listContent += createListContent(el.children);
+  });
+
+  return `<ul>${listContent}</ul>`;
+}
+
+function createList(title, list) {
+  document.body.innerHTML = `<h2>${title}</h2>${createListContent(list)}`;
+}
+
+//
+
+const list = [
+  {
+    value: 'Пункт 1.',
+    children: null,
+  },
+  {
+    value: 'Пункт 2.',
+    children: [
+      {
+        value: 'Подпункт 2.1.',
+        children: null,
+      },
+      {
+        value: 'Подпункт 2.2.',
+        children: [
+          {
+            value: 'Подпункт 2.2.1.',
+            children: null,
+          },
+          {
+            value: 'Подпункт 2.2.2.',
+            children: null,
+          },
+        ],
+      },
+      {
+        value: 'Подпункт 2.3.',
+        children: null,
+      },
+    ],
+  },
+  {
+    value: 'Пункт 3.',
+    children: null,
+  },
+];
+
+createList('TEST TITLE', list);
